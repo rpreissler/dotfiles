191baa643690f56a3bac244069a6ab0c816f9559
ryanburgess <burgessdryan@gmail.com>
2017-02-12 16:52:50 -0800

    add creative cloud download

---
+++ install/creative-cloud.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source ./utils.sh
+
+e_header "Installing Adobe Creative Cloud"
+
+wget https://creative.adobe.com/products/download/creative-cloud -P ~/Downloads/
+open ~/Downloads/creative-cloud
